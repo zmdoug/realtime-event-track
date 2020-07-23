90cfcc3656ebab3048d493c31c02851617ca1db7
Douglas Maehler <zmdoug@gmail.com>
2020-07-23 20:18:44 -0300

    feat: serve application home

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const app = require('express')();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 http.listen(port, function () {
   console.log('listening on *:' + port);
