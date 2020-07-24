c32b20fedd9016de5f12a6186ce1d31faf9f61dd
Douglas Maehler <zmdoug@gmail.com>
2020-07-24 02:16:40 -0300

    fix: local mongoDB uri

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -4,8 +4,8 @@ mongodbURI = () => {
   const MONGODB_PASS = process.env.MONGODB_PASS || null;
   const MONGODB_USER = process.env.MONGODB_USER || null;
   const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
-  // return `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`;
-  return `mongodb://teste:soheuse1@ds239911.mlab.com:39911/heroku_cvfrggt7`;
+  return `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`;
+  // return `mongodb://teste:soheuse1@ds239911.mlab.com:39911/heroku_cvfrggt7`;
 };
 
 module.exports = {
